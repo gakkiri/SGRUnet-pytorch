bbcc55ef8bb3f7df6070e2cd47b8c34e5b0cd44a
gakkii <35442235+gakkiri@users.noreply.github.com>
2020-04-15 21:20:29 +0800

    Add files via upload

--- inference.py
+++ inference.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 model_path = './checkpoints/best_checkpoint.pth'
-file_name = 'test.png'
+file_name = 'test2.png'
 file_path = f'./test/{file_name}'
 output_path = f'./output'
 
@@ -16,6 +16,7 @@ model = Gnet.SGRU().cuda()
 ckp = torch.load(model_path)
 model.load_state_dict(ckp['model'])
 model = model.eval()
+print('predict...')
 
 img = Image.open(file_path).convert('L').resize((256, 256))
 img = transforms.ToTensor()(img).unsqueeze(0)
@@ -25,10 +26,10 @@ with torch.no_grad():
     output = model(img).squeeze()  # [9, 3, H, W]
 
 output = output.detach().cpu().numpy()
-output = np.minimum(np.maximum(output, 0.0), 255.0)
+output = output.clip(0.0, 255.0)
 output = output.astype(np.uint8).transpose(0, 2, 3, 1)
 for i in range(output.shape[0]):
-##    img_fake_rgb = transforms.ToPILImage()(output[i, ...])
+    print(f'{i + 1}/{output.shape[0]} DONE!', f'result_{file_name.split(".")[0]}_{i}.png')
     img_fake_rgb = Image.fromarray(output[i, ...])
     # img_fake_rgb.show()
     img_fake_rgb.save(opj(output_path, f'result_{file_name.split(".")[0]}_{i}.png'))
